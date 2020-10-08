d91613679be40abef962c5fd195cea8c57b5fd28
himalaya-08 <himalaya@instantsys.com>
2020-10-08 18:20:16 +0530

    worked on the enhancement on record day screen

---
+++ bontriage/lib/view/RecordDayPage.dart
@@ -0,0 +1,290 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/util/constant.dart';
+
+class RecordDayPage extends StatefulWidget {
+  final bool hasData;
+
+  const RecordDayPage({Key key, this.hasData = false}) : super(key: key);
+
+  @override
+  _RecordDayPageState createState() => _RecordDayPageState();
+}
+
+class _RecordDayPageState extends State<RecordDayPage> {
+
+  GlobalKey _globalKey = GlobalKey();
+
+  List<Widget> _getSections() {
+    if (widget.hasData) {
+      return [
+        _getSectionWidget(
+          Constant.migraineIcon,
+          'Migrane',
+          '3:21 hours, Intensity: 4, Disability: 3',
+          '“Lorem ipsum dolor sit amet, consectetur adipiscing elit”',
+          '',
+        ),
+        _getSectionWidget(
+            Constant.sleepIcon,
+            'Sleep',
+            '7:47 hours, Woke up frequently',
+            '',
+            'This is worse than usual'),
+        _getSectionWidget(
+            Constant.waterDropIcon,
+            'Water',
+            '4 cups',
+            '',
+            'This is less than recommended'),
+        _getSectionWidget(Constant.mealIcon, 'Meals',
+            'Regular meal times', '', ''),
+        _getSectionWidget(
+            Constant.weatherIcon,
+            'Weather',
+            '90% humidity',
+            '',
+            'This is higher than usual'),
+        _getSectionWidget(Constant.pillIcon,
+            'Medication', 'Ibuprofen (200mg)', '', ''),
+        Text(
+          'Note:\n“Lorem ipsum dolor sit amet, consectetur adipiscing elit”',
+          style: TextStyle(
+              color: Constant.chatBubbleGreen60Alpha,
+              fontFamily: Constant.jostRegular,
+              fontWeight: FontWeight.w500,
+              fontSize: 14),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        FlatButton.icon(
+          padding: EdgeInsets.all(0),
+          onPressed: () {},
+          icon: Image.asset(
+            Constant.addCircleIcon,
+            width: 20,
+            height: 20,
+          ),
+          label: Text(
+            'Add/Edit Info',
+            style: TextStyle(
+                color: Constant.chatBubbleGreen,
+                fontFamily: Constant.jostRegular,
+                fontWeight: FontWeight.w500,
+                fontSize: 18),
+          ),
+          materialTapTargetSize:
+          MaterialTapTargetSize.shrinkWrap,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        FlatButton.icon(
+          padding: EdgeInsets.all(0),
+          onPressed: () {},
+          icon: Image.asset(
+            Constant.addCircleIcon,
+            width: 20,
+            height: 20,
+          ),
+          label: Text(
+            'Add/Edit Headache',
+            style: TextStyle(
+                color: Constant.chatBubbleGreen,
+                fontFamily: Constant.jostRegular,
+                fontWeight: FontWeight.w500,
+                fontSize: 18),
+          ),
+          materialTapTargetSize:
+          MaterialTapTargetSize.shrinkWrap,
+        ),
+      ];
+    } else {
+      return [
+        Text(
+          'Nothing Logged!',
+          style: TextStyle(
+            fontSize: 18,
+            color: Constant.chatBubbleGreen,
+            fontWeight: FontWeight.w500,
+            fontFamily: Constant.jostRegular
+          ),
+        ),
+        SizedBox(height: 5,),
+        Text(
+          'Add info to better personalize your experience',
+          style: TextStyle(
+              fontSize: 18,
+              color: Constant.chatBubbleGreen60Alpha,
+              fontWeight: FontWeight.w500,
+              fontFamily: Constant.jostRegular
+          ),
+        ),
+        Divider(
+          thickness: 0.5,
+          color: Constant.chatBubbleGreen,
+          height: 40,
+        ),
+        FlatButton.icon(
+          padding: EdgeInsets.all(0),
+          onPressed: () {},
+          icon: Image.asset(
+            Constant.addCircleIcon,
+            width: 20,
+            height: 20,
+          ),
+          label: Text(
+            'Add/Edit Info',
+            style: TextStyle(
+                color: Constant.chatBubbleGreen,
+                fontFamily: Constant.jostRegular,
+                fontWeight: FontWeight.w500,
+                fontSize: 18),
+          ),
+          materialTapTargetSize:
+          MaterialTapTargetSize.shrinkWrap,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        FlatButton.icon(
+          padding: EdgeInsets.all(0),
+          onPressed: () {},
+          icon: Image.asset(
+            Constant.addCircleIcon,
+            width: 20,
+            height: 20,
+          ),
+          label: Text(
+            'Add/Edit Headache',
+            style: TextStyle(
+                color: Constant.chatBubbleGreen,
+                fontFamily: Constant.jostRegular,
+                fontWeight: FontWeight.w500,
+                fontSize: 18),
+          ),
+          materialTapTargetSize:
+          MaterialTapTargetSize.shrinkWrap,
+        ),
+      ];
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final RenderBox renderBox = _globalKey.currentContext.findRenderObject();
+      final size = renderBox.size;
+      print(size);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      key: _globalKey,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: _getSections(),
+      ),
+    );
+  }
+
+  Widget _getSectionWidget(String imagePath, String headerText, String subText,
+      String noteText, String warningText) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              height: 38,
+              width: 38,
+              image: AssetImage(imagePath),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    headerText,
+                    style: TextStyle(
+                        color: Constant.chatBubbleGreen,
+                        fontFamily: Constant.jostRegular,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    subText,
+                    style: TextStyle(
+                        color: Constant.chatBubbleGreen60Alpha,
+                        fontFamily: Constant.jostRegular,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Visibility(
+                    visible: noteText.isNotEmpty,
+                    child: Text(
+                      'Note:\n$noteText',
+                      style: TextStyle(
+                          color: Constant.chatBubbleGreen60Alpha,
+                          fontFamily: Constant.jostRegular,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Visibility(
+                    visible: warningText.isNotEmpty,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image(
+                          width: 13,
+                          height: 13,
+                          image: AssetImage(Constant.warningPink),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          warningText,
+                          style: TextStyle(
+                            color: Constant.pinkTriggerColor,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: Constant.jostRegular,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Divider(
+          height: 30,
+          thickness: 0.5,
+          color: Constant.chatBubbleGreen,
+        ),
+      ],
+    );
+  }
+}
--- bontriage/lib/view/RecordDayScreen.dart
+++ bontriage/lib/view/RecordDayScreen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/util/Utils.dart';
 import 'package:mobile/util/constant.dart';
+import 'package:mobile/view/RecordDayPage.dart';
 
 class RecordDayScreen extends StatefulWidget {
   final DateTime dateTime;
@@ -14,37 +14,51 @@ class RecordDayScreen extends StatefulWidget {
 
 class _RecordDayScreenState extends State<RecordDayScreen> {
   DateTime _dateTime;
+  List<Widget> _pageViewWidgetList;
+  PageController _pageController;
+  int _currentPageIndex;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _dateTime = DateTime.now();
+    _currentPageIndex = 1;
+
+    _pageController = PageController(initialPage: _currentPageIndex);
+
+    _pageViewWidgetList = [
+      RecordDayPage(),
+      RecordDayPage(
+        hasData: true,
+      ),
+      RecordDayPage(),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          decoration: Constant.backgroundBoxDecoration,
-          child: LayoutBuilder(
-            builder: (context, viewPortConstraints) {
-              return SingleChildScrollView(
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minHeight: viewPortConstraints.maxHeight,
-                  ),
-                  child: SafeArea(
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(15, 20, 15, 0),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                      decoration: BoxDecoration(
-                        color: Constant.backgroundColor,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            topRight: Radius.circular(20)),
-                      ),
+      body: LayoutBuilder(
+        builder: (context, viewPortConstraints) {
+          return Container(
+            decoration: Constant.backgroundBoxDecoration,
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewPortConstraints.maxHeight,
+                ),
+                child: SafeArea(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(15, 20, 15, 0),
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                    decoration: BoxDecoration(
+                      color: Constant.backgroundColor,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20)),
+                    ),
+                    child: IntrinsicHeight(
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -102,94 +116,111 @@ class _RecordDayScreenState extends State<RecordDayScreen> {
                           SizedBox(
                             height: 40,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                _getSectionWidget(
-                                  Constant.migraineIcon,
-                                  'Migrane',
-                                  '3:21 hours, Intensity: 4, Disability: 3',
-                                  '“Lorem ipsum dolor sit amet, consectetur adipiscing elit”',
-                                  '',
-                                ),
-                                _getSectionWidget(
-                                    Constant.sleepIcon,
-                                    'Sleep',
-                                    '7:47 hours, Woke up frequently',
-                                    '',
-                                    'This is worse than usual'),
-                                _getSectionWidget(
-                                    Constant.waterDropIcon,
-                                    'Water',
-                                    '4 cups',
-                                    '',
-                                    'This is less than recommended'),
-                                _getSectionWidget(Constant.mealIcon, 'Meals',
-                                    'Regular meal times', '', ''),
-                                _getSectionWidget(
-                                    Constant.weatherIcon,
-                                    'Weather',
-                                    '90% humidity',
-                                    '',
-                                    'This is higher than usual'),
-                                _getSectionWidget(Constant.pillIcon,
-                                    'Medication', 'Ibuprofen (200mg)', '', ''),
-                                Text(
-                                  'Note:\n“Lorem ipsum dolor sit amet, consectetur adipiscing elit”',
-                                  style: TextStyle(
-                                      color: Constant.chatBubbleGreen60Alpha,
-                                      fontFamily: Constant.jostRegular,
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 14),
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                Row(),
-                                FlatButton.icon(
-                                  padding: EdgeInsets.all(0),
-                                  onPressed: () {},
-                                  icon: Image.asset(
-                                    Constant.addCircleIcon,
-                                    width: 20,
-                                    height: 20,
+                          Expanded(
+                            child: Container(
+                              height: 801,
+                              child: PageView.builder(
+                                itemCount: _pageViewWidgetList.length,
+                                controller: _pageController,
+                                onPageChanged: (index) {
+                                  setState(() {
+                                    print(index);
+                                  });
+                                },
+                                itemBuilder: (context, index) {
+                                  return _pageViewWidgetList[_currentPageIndex];
+                                },
+                              ),
+                              /*Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      _getSectionWidget(
+                                        Constant.migraineIcon,
+                                        'Migrane',
+                                        '3:21 hours, Intensity: 4, Disability: 3',
+                                        '“Lorem ipsum dolor sit amet, consectetur adipiscing elit”',
+                                        '',
+                                      ),
+                                      _getSectionWidget(
+                                          Constant.sleepIcon,
+                                          'Sleep',
+                                          '7:47 hours, Woke up frequently',
+                                          '',
+                                          'This is worse than usual'),
+                                      _getSectionWidget(
+                                          Constant.waterDropIcon,
+                                          'Water',
+                                          '4 cups',
+                                          '',
+                                          'This is less than recommended'),
+                                      _getSectionWidget(Constant.mealIcon, 'Meals',
+                                          'Regular meal times', '', ''),
+                                      _getSectionWidget(
+                                          Constant.weatherIcon,
+                                          'Weather',
+                                          '90% humidity',
+                                          '',
+                                          'This is higher than usual'),
+                                      _getSectionWidget(Constant.pillIcon,
+                                          'Medication', 'Ibuprofen (200mg)', '', ''),
+                                      Text(
+                                        'Note:\n“Lorem ipsum dolor sit amet, consectetur adipiscing elit”',
+                                        style: TextStyle(
+                                            color: Constant.chatBubbleGreen60Alpha,
+                                            fontFamily: Constant.jostRegular,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 14),
+                                      ),
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      Row(),
+                                      FlatButton.icon(
+                                        padding: EdgeInsets.all(0),
+                                        onPressed: () {},
+                                        icon: Image.asset(
+                                          Constant.addCircleIcon,
+                                          width: 20,
+                                          height: 20,
+                                        ),
+                                        label: Text(
+                                          'Add/Edit Info',
+                                          style: TextStyle(
+                                              color: Constant.chatBubbleGreen,
+                                              fontFamily: Constant.jostRegular,
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 18),
+                                        ),
+                                        materialTapTargetSize:
+                                        MaterialTapTargetSize.shrinkWrap,
+                                      ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      FlatButton.icon(
+                                        padding: EdgeInsets.all(0),
+                                        onPressed: () {},
+                                        icon: Image.asset(
+                                          Constant.addCircleIcon,
+                                          width: 20,
+                                          height: 20,
+                                        ),
+                                        label: Text(
+                                          'Add/Edit Headache',
+                                          style: TextStyle(
+                                              color: Constant.chatBubbleGreen,
+                                              fontFamily: Constant.jostRegular,
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 18),
+                                        ),
+                                        materialTapTargetSize:
+                                        MaterialTapTargetSize.shrinkWrap,
+                                      ),
+                                    ],
                                   ),
-                                  label: Text(
-                                    'Add/Edit Info',
-                                    style: TextStyle(
-                                        color: Constant.chatBubbleGreen,
-                                        fontFamily: Constant.jostRegular,
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 18),
-                                  ),
-                                  materialTapTargetSize:
-                                      MaterialTapTargetSize.shrinkWrap,
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                FlatButton.icon(
-                                  padding: EdgeInsets.all(0),
-                                  onPressed: () {},
-                                  icon: Image.asset(
-                                    Constant.addCircleIcon,
-                                    width: 20,
-                                    height: 20,
-                                  ),
-                                  label: Text(
-                                    'Add/Edit Headache',
-                                    style: TextStyle(
-                                        color: Constant.chatBubbleGreen,
-                                        fontFamily: Constant.jostRegular,
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 18),
-                                  ),
-                                  materialTapTargetSize:
-                                      MaterialTapTargetSize.shrinkWrap,
-                                ),
-                              ],
+                                ),*/
                             ),
                           ),
                         ],
@@ -197,104 +228,11 @@ class _RecordDayScreenState extends State<RecordDayScreen> {
                     ),
                   ),
                 ),
-              );
-            },
-          )),
-    );
-  }
-
-  Widget _getSectionWidget(String imagePath, String headerText, String subText,
-      String noteText, String warningText) {
-    return Column(
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image(
-              height: 38,
-              width: 38,
-              image: AssetImage(imagePath),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    headerText,
-                    style: TextStyle(
-                        color: Constant.chatBubbleGreen,
-                        fontFamily: Constant.jostRegular,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18),
-                  ),
-                  SizedBox(
-                    height: 2,
-                  ),
-                  Text(
-                    subText,
-                    style: TextStyle(
-                        color: Constant.chatBubbleGreen60Alpha,
-                        fontFamily: Constant.jostRegular,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Visibility(
-                    visible: noteText.isNotEmpty,
-                    child: Text(
-                      'Note:\n$noteText',
-                      style: TextStyle(
-                          color: Constant.chatBubbleGreen60Alpha,
-                          fontFamily: Constant.jostRegular,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Visibility(
-                    visible: warningText.isNotEmpty,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Image(
-                          width: 13,
-                          height: 13,
-                          image: AssetImage(Constant.warningPink),
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          warningText,
-                          style: TextStyle(
-                            color: Constant.pinkTriggerColor,
-                            fontSize: 12,
-                            fontWeight: FontWeight.w400,
-                            fontFamily: Constant.jostRegular,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
               ),
             ),
-          ],
-        ),
-        Divider(
-          height: 30,
-          thickness: 0.5,
-          color: Constant.chatBubbleGreen,
-        ),
-      ],
+          );
+        },
+      ),
     );
   }
 }
