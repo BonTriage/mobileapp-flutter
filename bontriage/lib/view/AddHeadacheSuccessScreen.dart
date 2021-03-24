cfa8f21a5eb2a3911200567e6ffcd1597279ac73
DikshantRawal <dikshantrawal@gmail.com>
2021-03-24 14:54:17 +0530

    Worked on AddHeadache sucess screen issue

--- bontriage/lib/view/AddHeadacheSuccessScreen.dart
+++ bontriage/lib/view/AddHeadacheSuccessScreen.dart
@@ -18,126 +18,123 @@ class _AddHeadacheSuccessScreenState extends State<AddHeadacheSuccessScreen> {
         Navigator.popUntil(context, ModalRoute.withName(Constant.homeRouter));
         return false;
       },
-      child: MediaQuery(
-
-        child: Scaffold(
-          body: Container(
-            decoration: Constant.backgroundBoxDecoration,
-            child: SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                    minHeight: MediaQuery.of(context).size.height
-                ),
-                child: SafeArea(
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(15, 20, 15, 0),
-                    decoration: BoxDecoration(
-                      color: Constant.backgroundColor,
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Align(
-                            alignment: Alignment.topRight,
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.popUntil(context, ModalRoute.withName(Constant.homeRouter));
-                              },
-                              child: Image(
-                                image: AssetImage(Constant.closeIcon),
-                                width: 22,
-                                height: 22,
-                              ),
+      child: Scaffold(
+        body: Container(
+          decoration: Constant.backgroundBoxDecoration,
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                  minHeight: MediaQuery.of(context).size.height
+              ),
+              child: SafeArea(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(15, 20, 15, 0),
+                  decoration: BoxDecoration(
+                    color: Constant.backgroundColor,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.popUntil(context, ModalRoute.withName(Constant.homeRouter));
+                            },
+                            child: Image(
+                              image: AssetImage(Constant.closeIcon),
+                              width: 22,
+                              height: 22,
                             ),
                           ),
-                          SizedBox(height: 80,),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 30),
-                            child: Align(
-                              alignment: Alignment.center,
-                              child: Text(
-                                Constant.headacheAdded,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Constant.chatBubbleGreen,
-                                    fontFamily: Constant.jostMedium
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 20,),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 30),
-                            child: Align(
-                              alignment: Alignment.center,
-                              child: Text(
-                                Constant.logYourDayToAssess,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: Constant.chatBubbleGreen,
-                                    fontFamily: Constant.jostMedium
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 80,),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 50),
-                            child: BouncingWidget(
-                              onPressed: () {
-                                Navigator.pushReplacementNamed(context, Constant.logDayScreenRouter);
-                              },
-                              child: Container(
-                                padding: EdgeInsets.symmetric(vertical: 10),
-                                decoration: BoxDecoration(
+                        ),
+                        SizedBox(height: 80,),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 30),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              Constant.headacheAdded,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 18,
                                   color: Constant.chatBubbleGreen,
-                                  borderRadius: BorderRadius.circular(30),
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    Constant.logDay,
-                                    style: TextStyle(
-                                        color: Constant.bubbleChatTextView,
-                                        fontSize: 15,
-                                        fontFamily: Constant.jostMedium),
-                                  ),
+                                  fontFamily: Constant.jostMedium
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 30),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              Constant.logYourDayToAssess,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Constant.chatBubbleGreen,
+                                  fontFamily: Constant.jostMedium
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 80,),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 50),
+                          child: BouncingWidget(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(context, Constant.logDayScreenRouter);
+                            },
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 10),
+                              decoration: BoxDecoration(
+                                color: Constant.chatBubbleGreen,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  Constant.logDay,
+                                  style: TextStyle(
+                                      color: Constant.bubbleChatTextView,
+                                      fontSize: 15,
+                                      fontFamily: Constant.jostMedium),
                                 ),
                               ),
                             ),
                           ),
-                          SizedBox(height: 20,),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 50),
-                            child: BouncingWidget(
-                              onPressed: () {
-                                _popAndSaveDataInSharePreference();
-                              },
-                              child: Container(
-                                padding: EdgeInsets.symmetric(vertical: 10),
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                      width: 1.3, color: Constant.chatBubbleGreen),
-                                  borderRadius: BorderRadius.circular(30),
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    Constant.viewTrends,
-                                    style: TextStyle(
-                                        color: Constant.chatBubbleGreen,
-                                        fontSize: 15,
-                                        fontFamily: Constant.jostMedium),
-                                  ),
+                        ),
+                        SizedBox(height: 20,),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 50),
+                          child: BouncingWidget(
+                            onPressed: () {
+                              _popAndSaveDataInSharePreference();
+                            },
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 10),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 1.3, color: Constant.chatBubbleGreen),
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  Constant.viewTrends,
+                                  style: TextStyle(
+                                      color: Constant.chatBubbleGreen,
+                                      fontSize: 15,
+                                      fontFamily: Constant.jostMedium),
                                 ),
                               ),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
