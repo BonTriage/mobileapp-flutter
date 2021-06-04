89eb24b81f0b860df13ab3a762440189ee239d22
DikshantRawal <dikshantrawal@gmail.com>
2021-06-04 19:26:37 +0530

    Worked on More setting screen changes for Phone Permission

--- bontriage/lib/view/MoreSettingScreen.dart
+++ bontriage/lib/view/MoreSettingScreen.dart
@@ -1,19 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:mobile/providers/SignUpOnBoardProviders.dart';
 import 'package:mobile/util/constant.dart';
 import 'package:mobile/view/MoreSection.dart';
 
+import '../main.dart';
+
 class MoreSettingScreen extends StatefulWidget {
   final Future<dynamic> Function(BuildContext, String, dynamic) onPush;
 
-  const MoreSettingScreen({Key key, this.onPush})
-      : super(key: key);
+  const MoreSettingScreen({Key key, this.onPush}) : super(key: key);
+
   @override
   _MoreSettingScreenState createState() => _MoreSettingScreenState();
 }
 
 class _MoreSettingScreenState extends State<MoreSettingScreen> {
-
   String _notificationStatus = Constant.notAllowed;
 
   @override
@@ -26,83 +30,82 @@ class _MoreSettingScreenState extends State<MoreSettingScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: Constant.backgroundBoxDecoration,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: MediaQuery.of(context).size.height,
-            ),
-            child: SingleChildScrollView(
-              child: SafeArea(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 40,
-                    ),
-                    GestureDetector(
-                      behavior: HitTestBehavior.translucent,
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Constant.moreBackgroundColor,
-                        ),
-                        child: Row(
-                          children: [
-                            Image(
-                              width: 16,
-                              height: 16,
-                              image: AssetImage(Constant.leftArrow),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              'Settings',
-                              style: TextStyle(
-                                  color: Constant.locationServiceGreen,
-                                  fontSize: 16,
-                                  fontFamily: Constant.jostRegular),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Container(
+      decoration: Constant.backgroundBoxDecoration,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+          ),
+          child: SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 40,
+                  ),
+                  GestureDetector(
+                    behavior: HitTestBehavior.translucent,
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Container(
                       padding:
                           EdgeInsets.symmetric(horizontal: 15, vertical: 15),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
                         color: Constant.moreBackgroundColor,
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      child: Row(
                         children: [
-                          MoreSection(
-                            currentTag: Constant.notifications,
-                            text: Constant.notifications,
-                            moreStatus: _notificationStatus,
-                            isShowDivider: false,
-                            navigateToOtherScreenCallback: _navigateToOtherScreen,
+                          Image(
+                            width: 16,
+                            height: 16,
+                            image: AssetImage(Constant.leftArrow),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Settings',
+                            style: TextStyle(
+                                color: Constant.locationServiceGreen,
+                                fontSize: 16,
+                                fontFamily: Constant.jostRegular),
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Constant.moreBackgroundColor,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        MoreSection(
+                          currentTag: Constant.notifications,
+                          text: Constant.notifications,
+                          moreStatus: _notificationStatus,
+                          isShowDivider: false,
+                          navigateToOtherScreenCallback: _navigateToOtherScreen,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
         ),
-      );
+      ),
+    );
   }
 
   void _navigateToOtherScreen(String routeName, dynamic arguments) async {
@@ -111,16 +114,38 @@ class _MoreSettingScreenState extends State<MoreSettingScreen> {
   }
 
   void _checkNotificationStatus() async {
-    var notificationListData = await SignUpOnBoardProviders.db.getAllLocalNotificationsData();
+    var notificationListData =
+        await SignUpOnBoardProviders.db.getAllLocalNotificationsData();
 
-    if(notificationListData == null || notificationListData.isEmpty) {
-      setState(() {
-        _notificationStatus = Constant.notAllowed;
-      });
+    if (Platform.isIOS) {
+      var permissionResult = await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              IOSFlutterLocalNotificationsPlugin>()
+          ?.requestPermissions(
+            alert: true,
+            badge: true,
+            sound: true,
+          );
+
+      if (permissionResult ?? true) {
+        setState(() {
+          _notificationStatus = Constant.notAllowed;
+        });
+      } else {
+        setState(() {
+          _notificationStatus = Constant.allowed;
+        });
+      }
     } else {
-      setState(() {
-        _notificationStatus = Constant.allowed;
-      });
+      if (notificationListData == null || notificationListData.isEmpty) {
+        setState(() {
+          _notificationStatus = Constant.notAllowed;
+        });
+      } else {
+        setState(() {
+          _notificationStatus = Constant.allowed;
+        });
+      }
     }
   }
 }
